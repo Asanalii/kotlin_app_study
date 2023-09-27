85ef9bf37262852e75c03b3c8c9d49d3e9074608
Asanalii <asanaliiospannn@gmail.com>
2023-09-27 07:54:58 +0600

    first commit

---
+++ app/src/main/java/kz/just_code/helloApp/MainActivity.kt
@@ -0,0 +1,11 @@
+ package kz.just_code.helloApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
